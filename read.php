91f000ebf88d721f6dab1f7d810edc6d5669ba4b
JayRawlani <46822412+JayRawlani@users.noreply.github.com>
2020-11-02 13:33:03 +0530

    .

--- read.php
+++ read.php
@@ -38,7 +38,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="form.php">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item active">
         <a class="nav-link" href="read.php">Registrant list</a>
